1615fa8afb5fa04a5c45b382dc7751d53e223e57
seinokoula <alexandre.charles92@gmail.com>
2022-11-15 15:29:59 +0100

    :construction: test for mobile game

--- main.js
+++ main.js
@@ -141,4 +141,17 @@ if (typeof particlesJS !== 'undefined') {
     });
   });
 
-  setTimeout( function () { alert( "coucou fils de pute" ); }, 120000 );
+setTimeout(function () { alert("ALAID JAI PAS LA SOLUCE NON PLUS"); }, 120000);
+  
+const linkText = document.querySelector('.link-text');
+const linkImage = document.querySelector('.link-image');
+
+function showImgContent(e) {
+  x = e.clientX;
+  y = e.clientY;
+  linkImage.style.transform = `translate3d(${x}px, ${y}px, 0)`;
+  linkText.style.setProperty('--x',(x)+'px');
+  linkText.style.setProperty('--y',(y)+'px');
+}
+
+document.addEventListener('mousemove', showImgContent);
