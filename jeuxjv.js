79593b3d7ba50e57fd0399361ff270c69685455f
Louis Perrenot <louis.perrenot@edu.devinci.net>
2022-11-18 09:56:49 +0100

    Commit PP The Frog

--- jeuxjv.js
+++ jeuxjv.js
@@ -11,16 +11,36 @@ function getChoice(e) {
         goodChoice()
     } else if (document.getElementById('img').src.includes('chaise') && e.target.id === 'choice2') {
         goodChoice()
-    } else (
+    } else if (document.getElementById('img').src.includes('clavier') && e.target.id === 'choice3') {
+        goodChoice()
+    } else if (document.getElementById('img').src.includes('lampe') && e.target.id === 'choice1') {
+        goodChoice()
+    } else {
         wrongChoice()
-    )
+    }
 }
 
 function goodChoice() {
     if (document.getElementById('img').src.includes('dashboard')) {
-        document.getElementById('img').src = './img/box0001-0100.png'
+        showGoodButton()
+        setTimeout(function () {
+            document.getElementById('img').src = './img/box0001-0100.gif'
+        }, 600)
     } else if (document.getElementById('img').src.includes('box')) {
-        document.getElementById('img').src = './img/chaise0001-0100_1.png'
+        showGoodButton()
+        setTimeout(function() {
+            document.getElementById('img').src = './img/chaise0001-0100_1.gif'
+        }, 600)
+    } else if (document.getElementById('img').src.includes('chaise')) {
+        showGoodButton()
+        setTimeout(function() {
+            document.getElementById('img').src = './img/clavier0001-0100.gif'
+        }, 600)
+    } else if (document.getElementById('img').src.includes('clavier')) {
+        showGoodButton()
+        setTimeout(function() {
+            document.getElementById('img').src = './img/lampe0001-0100.gif'
+        }, 600)
     } else {
         if (window.location.pathname.includes('InterAxe_IIM')) {
             document.location.href = '/InterAxe_IIM/jvsucceed.html'
@@ -29,8 +49,14 @@ function goodChoice() {
     }
 }
 
+function showGoodButton() {
+    document.getElementById('goodChoice').classList.add('active')
+    setTimeout(function () {
+        document.getElementById('goodChoice').classList.remove('active')
+    }, 600)
+}
+
 function wrongChoice() {
-    console.log('Pas bon')
     document.getElementById('wrongChoice').classList.add('active')
     setTimeout(function () { 
         document.getElementById('wrongChoice').classList.remove('active')
